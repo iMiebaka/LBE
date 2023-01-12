e87f810fdcc8145aba6463171ecc465529adaa07
imiebaka <triple07even@gmail.com>
2023-01-12 22:55:37 +0100

    contrab removed

--- src/config/mockServer.ts
+++ src/config/mockServer.ts
@@ -1,13 +1,12 @@
 import express, { Application } from "express"
 import { apiV1 } from "../routes"
-import "./contrab";
+// import "./contrab"; // Disable to run on external system
 import "../database/db"
 
 function defaultConnect() {
     const app: Application = express();
     app.use(express.json())
     app.use("/api/v1", apiV1);
-
     return app
 }
 
